49d045444c3a04ea879c99a5f097cfce61992da8
round48z-sketch <round48z@gmail.com>
2026-09-24 18:55:04 +0900

    Add files via upload

--- components/home/GalleryTeaser.tsx
+++ components/home/GalleryTeaser.tsx
@@ -9,6 +9,11 @@ export function GalleryTeaser() {
 
   return (
     <section className="gallery-band" id="gallery">
+      <div className="gallery-ornament" aria-hidden="true">
+        <span className="gallery-ornament__star a" />
+        <span className="gallery-ornament__star b" />
+        <span className="gallery-ornament__line" />
+      </div>
       <div className="container">
         <Reveal>
           <SectionHead en="Gallery" ja="ギャラリー" href="/gallery" lead="笑顔と、日常と、季節の光。" />
@@ -16,20 +21,22 @@ export function GalleryTeaser() {
       </div>
       <Reveal className="gallery-spread">
         {images.map((image, index) => (
-          <figure key={image.src} className={`gallery-spread__item n${index + 1}`}>
+          <figure key={image.src} className={`gallery-spread__item n${index + 1} is-${image.layout}`}>
             <Image
               src={image.src}
               alt={image.alt}
               width={image.width}
               height={image.height}
-              sizes="(min-width: 980px) 42vw, 90vw"
+              sizes="(min-width: 980px) 42vw, (min-width: 720px) 55vw, 92vw"
             />
           </figure>
         ))}
       </Reveal>
       <div className="container">
-        <Reveal className="section-foot">
-          <TextLink href="/gallery">View More</TextLink>
+        <Reveal className="section-foot gallery-foot">
+          <TextLink href="/gallery" className="gallery-more">
+            View More
+          </TextLink>
         </Reveal>
       </div>
     </section>
